e1e4b035e3d46f132e6ace86f249912eb135605b
s21010 <s21010@std.it-college.ac.jp>
2023-08-10 10:47:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.firebaseauth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textFullname: TextView
+    lateinit var textEmail: TextView
+
+    val firebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textFullname = findViewById(R.id.full_name)
+        textEmail = findViewById(R.id.email)
+
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser!=null){
+            textFullname.text = firebaseUser.displayName
+            textEmail.text = firebaseUser.email
+        }else{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+}
