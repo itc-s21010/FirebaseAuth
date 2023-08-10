4bf5e366ee566d45a97ff994404f0db9ff3d86f2
s21010 <s21010@std.it-college.ac.jp>
2023-08-10 12:01:16 +0900

    ログイン機能

--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.firebaseauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     lateinit var textFullname: TextView
     lateinit var textEmail: TextView
+    lateinit var btnLogout: Button
 
     val firebaseAuth = FirebaseAuth.getInstance()
 
@@ -17,12 +19,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textFullname = findViewById(R.id.full_name)
         textEmail = findViewById(R.id.email)
+        btnLogout = findViewById(R.id.btn_logout)
 
         val firebaseUser = firebaseAuth.currentUser
         if (firebaseUser!=null){
             textFullname.text = firebaseUser.displayName
             textEmail.text = firebaseUser.email
         }else{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        btnLogout.setOnClickListener {
+            firebaseAuth.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
