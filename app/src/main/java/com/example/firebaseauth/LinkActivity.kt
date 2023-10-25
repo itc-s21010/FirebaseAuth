92e1a8132f5fbd47c7d4c07183a7b09231273199
s21022 <s21022@std.it-college.ac.jp>
2023-10-25 16:36:32 +0900

    緊急時連絡リンクページ作成、編集

--- app/src/main/java/com/example/firebaseauth/LinkActivity.kt
+++ app/src/main/java/com/example/firebaseauth/LinkActivity.kt
@@ -1,11 +1,35 @@
 package com.example.firebaseauth
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LinkActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var button2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_link)
+
+        button = findViewById(R.id.button)
+        button = findViewById(R.id.button2)
+
+        button.setOnClickListener {
+            openUrl("https://chat.google.com/dm/m8MplEAAAAE/kLxdCDDvkAw/kLxdCDDvkAw?cls=10")
+        }
+
+        button2.setOnClickListener {
+            openUrl("")
+        }
+
+    }
+
+    private fun openUrl(link:String) {
+        val uri = Uri.parse(link)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+
     }
 }
